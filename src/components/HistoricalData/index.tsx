b1dd62a0d3f9ca1fb785cac11f96ffcc1a15eb23
Raluca Nicola <ralucagnicola@gmail.com>
2023-11-24 23:39:18 +0100

    Add legend for the bike routes

--- src/components/HistoricalData/index.tsx
+++ src/components/HistoricalData/index.tsx
@@ -5,6 +5,33 @@ import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { CalciteLabel, CalciteSwitch } from '@esri/calcite-components-react';
 import { toggleAccidentsLayer, toggleRoutesLayer, toggleConstructionsLayer } from '../../store/historicalSlice';
 
+const Legend = () => {
+  return (
+    <>
+      <div className={styles.legendSymbol}>
+        <div style={{ borderBottom: '1px solid #F7FBFF', width: '100px', height: 0 }}></div>
+        <div> 1 - 9 routes</div>
+      </div>
+      <div className={styles.legendSymbol}>
+        <div style={{ borderBottom: '2px solid #C6DBEF', width: '100px', height: 0 }}></div>
+        <div> 10 - 24 routes</div>
+      </div>
+      <div className={styles.legendSymbol}>
+        <div style={{ borderBottom: '3px solid #6BAED6', width: '100px', height: 0 }}></div>
+        <div> 25 - 45 routes</div>
+      </div>
+      <div className={styles.legendSymbol}>
+        <div style={{ borderBottom: '5px solid #2171B5', width: '100px', height: 0 }}></div>
+        <div> 46 - 87 routes</div>
+      </div>
+      <div className={styles.legendSymbol}>
+        <div style={{ borderBottom: '6px solid #08306B', width: '100px', height: 0 }}></div>
+        <div> 88 - 147 routes</div>
+      </div>
+    </>
+  );
+};
+
 const HistoricalData = () => {
   const accidentsVisible = useAppSelector((state: RootState) => state.historical.accidentsLayerVisible);
   const routesVisible = useAppSelector((state: RootState) => state.historical.routesLayerVisible);
@@ -13,28 +40,35 @@ const HistoricalData = () => {
 
   return (
     <div className={styles.container}>
-      <CalciteLabel layout='inline-space-between' scale='l'>
-        Bicycle accidents{' '}
-        <CalciteSwitch
-          checked={accidentsVisible ? true : undefined}
-          onCalciteSwitchChange={() => dispatch(toggleAccidentsLayer())}
-        ></CalciteSwitch>
-      </CalciteLabel>
-      <div className={styles.note}>Historical accident data from January 2015 until October 2023.</div>
-      <CalciteLabel layout='inline-space-between' scale='l'>
-        Bicycle routes{' '}
-        <CalciteSwitch
-          checked={routesVisible ? true : undefined}
-          onCalciteSwitchChange={() => dispatch(toggleRoutesLayer())}
-        ></CalciteSwitch>
-      </CalciteLabel>
-      <CalciteLabel layout='inline-space-between' scale='l'>
-        Construction sites{' '}
-        <CalciteSwitch
-          checked={constructionsVisible ? true : undefined}
-          onCalciteSwitchChange={() => dispatch(toggleConstructionsLayer())}
-        ></CalciteSwitch>
-      </CalciteLabel>
+      <div>
+        <CalciteLabel layout='inline-space-between' scale='l'>
+          Bicycle accidents{' '}
+          <CalciteSwitch
+            checked={accidentsVisible ? true : undefined}
+            onCalciteSwitchChange={() => dispatch(toggleAccidentsLayer())}
+          ></CalciteSwitch>
+        </CalciteLabel>
+        <div className={styles.note}>Historical accident data from January 2015 until October 2023.</div>
+      </div>
+      <div>
+        <CalciteLabel layout='inline-space-between' scale='l'>
+          Bicycle routes{' '}
+          <CalciteSwitch
+            checked={routesVisible ? true : undefined}
+            onCalciteSwitchChange={() => dispatch(toggleRoutesLayer())}
+          ></CalciteSwitch>
+        </CalciteLabel>
+        {routesVisible && <Legend></Legend>}
+      </div>
+      <div>
+        <CalciteLabel layout='inline-space-between' scale='l'>
+          Construction sites{' '}
+          <CalciteSwitch
+            checked={constructionsVisible ? true : undefined}
+            onCalciteSwitchChange={() => dispatch(toggleConstructionsLayer())}
+          ></CalciteSwitch>
+        </CalciteLabel>
+      </div>
     </div>
   );
 };
